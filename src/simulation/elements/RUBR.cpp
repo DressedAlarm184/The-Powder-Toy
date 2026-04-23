85a986044113560c31cf7fa487b228e361eea366
DressedAlarm184 <155406555+DressedAlarm184@users.noreply.github.com>
2026-04-22 19:12:12 -0700

    Add new RUBR (Rubber) element

---
+++ src/simulation/elements/RUBR.cpp
@@ -0,0 +1,28 @@
+#include "simulation/ElementCommon.h"
+
+void Element::Element_RUBR() {
+	Identifier = "DEFAULT_PT_RUBR";
+	Name = "RUBR";
+	Colour = 0x112224_rgb;
+	MenuVisible = 1;
+	MenuSection = SC_SOLIDS;
+	Enabled = 1;
+
+	Falldown = 0;
+
+	Flammable = 5;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 2;
+
+	HeatConduct = 20;
+	Description = "Rubber. Insulating. Can melt. Tears under pressure.";
+
+	Properties = TYPE_PART;
+
+	HighTemperature = 453.15;
+	HighTemperatureTransition = PT_LAVA;
+
+	// HighPressure = 255;
+	// HighPressureTransition = 
+}
